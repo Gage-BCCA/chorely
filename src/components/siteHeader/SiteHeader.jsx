5077d02ad4ba79148a68b04d8b3f8ece3949afdb
Gage <gschaffer@basecampcodeacademy.org>
2024-10-12 12:20:05 -0500

    First push.

---
+++ src/components/siteHeader/SiteHeader.jsx
@@ -0,0 +1,11 @@
+import './siteHeader.css'
+
+export default function SiteHeader(){
+    return(
+        <>
+            <div className='site-header'>
+                <h1>Hello World</h1>
+            </div>
+        </>
+    )
+}
