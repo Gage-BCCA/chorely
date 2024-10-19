793e0991dc59947c63078d45e78f623f05ad6a6a
Quan <jbuford@basecampcodingacademy.org>
2024-10-19 10:06:06 -0500

    quan push 4.5

--- src/components/deletionForm/DeletionForm.jsx
+++ src/components/deletionForm/DeletionForm.jsx
@@ -17,12 +17,12 @@ export default function DeletionForm (chore) {
             <h3>Remove chore below!</h3>
             <label>
              Name:
-              <input type="text" name="name" defaultValue={chore.name}/>
+              <input type="text" name="name" />
              </label>
                 <br />
             <label>
              Chore:
-             <input type="text" name="chore" defaultValue={chore.chore}/>
+             <input type="text" name="chore" />
              </label>
              <br />
              <label>
