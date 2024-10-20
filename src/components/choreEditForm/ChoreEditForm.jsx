c5872373e1f0c35ffe4dba2e61196f6319ca487f
danielchristy <daaronchristy@gmail.com>
2024-10-19 23:10:19 -0500

    fixed Delete Functionality  in DetailsPopup, fixed Update functionality in ChoreEditForm.

--- src/components/choreEditForm/ChoreEditForm.jsx
+++ src/components/choreEditForm/ChoreEditForm.jsx
@@ -1,42 +1,72 @@
 import './choreEditForm.css';
 
-// Styling
-// Input Validation!!
-// Where are comments being input?
+export default function ChoreEditForm({ chore, handlePopup }) {
+    console.log('loaded');
 
-export default function ChoreEditForm({ chore, handlePopup, handleUpdate }) {
+    const handleUpdate = async (event) => {
+        console.log('submit button clicked');
+        event.preventDefault();
+
+        const updatedChoreInfo = {
+            "chore": event.target.chore.value,
+            "owner": event.target.owner.value,
+            "date_posted": event.target.date.value,
+            "importance": event.target.importance.value,
+            "due_date": event.target.dueDate.value,
+            "status": event.target.status.value,
+            "completed": false,
+            "category": "Outdoor",
+            "description": event.target.description.value,
+            "allowance_amount": event.target.allowance.value,
+            "paid": parseInt(event.target.allowance.value) ? true : false,
+            "comments": []
+        }
+
+        console.log(updatedChoreInfo)
+
+        fetch(`https://unit-4-project-app-24d5eea30b23.herokuapp.com/update/data?teamId=2&recordId=${chore.id}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(updatedChoreInfo)})
+        .then((response) => {
+            console.log('Update Status: ', response.status)})
+        .catch(err => console.log('rejected: ', err.message))
+    }
+    
     return (
         <div className='popup'>
             <div className='edit-chore-details'>
                 <h1 className='edit-details-header'>Editing: {chore.chore}</h1>
-                <form>
+                <form onSubmit={handleUpdate}>
                     <label>
                         <span>Chore name:</span>
-                        <input type="text" newChore="chore" defaultValue={chore.chore} />
+                        <input type="text" defaultValue={chore.chore} name="chore" />
                     </label>
                     <label>
                         <span>Owner:</span>
-                        <input type="text" newOwner="owner" defaultValue={chore.owner} />
+                        <input type="text" defaultValue={chore.owner} name="owner" />
                     </label>
                     <label>
                         <span>Date Posted:</span>
-                        <input type="date" newDatePosted="date-posted" defaultValue={chore.date_posted} />
+                        <input type="date" defaultValue={chore.date_posted} name="date" />
                     </label>
                     <label>
                         <span>Importance:</span>
-                        <select newImportance="importance" defaultValue={chore.importance}>
-                            <option value='Low'>Low</option>
-                            <option value='Medium'>Medium</option>
-                            <option value='High'>High</option>
+                        <select defaultValue={chore.status} name="importance">
+                            <option value='Not Started'>Low</option>
+                            <option value='In Progress'>Medium</option>
+                            <option value='Completed'>High</option>
                         </select>
                     </label>
                     <label>
                         <span>Due Date:</span>
-                        <input type="date" newDueDate="due-date" defaultValue={chore.due_date} />
+                        <input type="date" defaultValue={chore.due_date} name="dueDate"/>
                     </label>
                     <label>
                         <span>Status:</span>
-                        <select newStatus="status" defaultValue={chore.status}>
+                        <select defaultValue={chore.status} name="status">
                             <option value='Not Started'>Not Started</option>
                             <option value='In Progress'>In Progress</option>
                             <option value='Completed'>Completed</option>
@@ -44,31 +74,25 @@ export default function ChoreEditForm({ chore, handlePopup, handleUpdate }) {
                     </label>
                     <label>
                         <span>Description:</span>
-                        <input type="text" newDescription="description" defaultValue={chore.description} />
+                        <input type="text" defaultValue={chore.description} name="description" />
                     </label>
                     <label>
                         <span>Allowance Amount:</span>
-                        <input type="text" newAllowanceAmount="allowance-amount" defaultValue={chore.allowance_amount} />
+                        <input type="text" defaultValue={chore.allowance_amount} name="allowance" />
                     </label>
                     <label>
                         <span>Paid:</span>
-                        <select newPaid="paid" defaultValue={chore.paid ? 'Yes' : 'No'}>
+                        <select defaultValue={chore.paid ? 'Yes' : 'No'}>
                             <option value='No'>No</option>
                             <option value='Yes'>Yes</option>
                         </select>
                     </label>
-                </form>
-                <div className='update-btns'>
+                    <div className='update-btns'>
                     <button type='submit'>Submit Changes</button>
-                    <button onClick={() => handlePopup("close")}>Cancel</button>
+                    <button onClick={ () => handlePopup('close')}>Cancel</button>
                 </div>
+                </form>
             </div>
         </div>
     )
-
-    function handleUpdate () {
-        // PUT updated values into database - merge with previous
-    }
-
-
-}
+};
